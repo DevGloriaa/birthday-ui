141cf1d55a23bda0863312aa1c82ff5be6628d6e
Gloria-Obiorah <obiorahgloria911@gmail.com>
2025-12-31 15:53:22 +0100

    fix: routing to include landing page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing"; 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
@@ -7,7 +8,13 @@ import AddBirthday from "./pages/AddBirthday";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+  
+      <Route path="/" element={<Landing />} />
+      
+    
+      <Route path="/login" element={<Login />} />
+      
+   
       <Route path="/signup" element={<Signup />} />
       <Route path="/home" element={<Home />} />
       <Route path="/add" element={<AddBirthday />} />
