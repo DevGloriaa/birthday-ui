e7fe4b931472c75bf8593e36697af42742f4323e
Gloria-Obiorah <obiorahgloria911@gmail.com>
2026-01-01 22:55:03 +0100

    fix: protected routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing"; 
+import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
@@ -7,18 +7,17 @@ import AddBirthday from "./pages/AddBirthday";
 
 function App() {
   return (
-    <Routes>
-  
-      <Route path="/" element={<Landing />} />
-      
-    
-      <Route path="/login" element={<Login />} />
-      
-   
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/add" element={<AddBirthday />} />
-    </Routes>
+    // <BrowserRouter>  <-- REMOVE THIS WRAPPER
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        
+        {/* Protected Routes */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/add" element={<AddBirthday />} />
+      </Routes>
+    // </BrowserRouter> <-- REMOVE THIS WRAPPER
   );
 }
 
